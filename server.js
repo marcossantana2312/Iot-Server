9d5328759b518eeb2adb4c05adc0eac809a75352
Marcos da Silva Santana <marcos.santana@cubos.io>
2020-08-23 11:52:33 -0300

    feat: show message sended by query string

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require("http");
+const url = require("url");
 
 http.createServer((req, res) => {
-	res.write("Hello Client")
+
+	const { message } = url.parse(req.url, true).query;
+	
+	if (message) {
+		res.write(`This is your message: ${message}`)
+	} else {
+		res.write("Send me a message by query string")
+	}
 	res.end();
 }).listen(4000, () => {
 	console.log("Server ready")
